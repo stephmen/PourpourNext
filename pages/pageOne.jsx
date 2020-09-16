6880e9a0df65c53a90076892f177e103418485da
stephmen <stephmen@gmail.com>
2020-09-16 15:53:40 -0400

    Menu Material Design Working

---
+++ pages/pageOne.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Menu from "../components/Menu"
+//import { Burger, Menu } from '../components';
+
+const firstPage = (props) => {
+   
+    return(
+    <div>
+    <header className="header1">
+        <div className="home">
+            <img src="Vector.png" alt="home"/>
+        </div>
+        <div className="nav">
+            
+            <Menu />
+        </div>
+    </header>
+     <main>
+        <img className="logo" src="Pourpour_Logo.png" /> 
+
+    </main>
+    <img className="photo" src="PlacedesArtsPP1.jpg" />
+    <footer className="footer">
+        <div className="Left"></div>
+        <div className="bottomIcon">
+            <img src="Courrier.png" alt=""/>
+            <text>"               "</text>
+            <img src="facebook.png" alt=""/>
+            <text>"               "</text>
+            <img src="Youtube.png" alt=""/>
+            <text>"               "</text>
+            <img src="twitter.png" alt=""/>
+        </div>
+    </footer>
+    </div>
+    )
+    
+}
+
+export default firstPage
